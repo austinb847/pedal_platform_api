1156c63393da35958a5885777cf642524153197f
austinb847 <austinb847@icloud.com>
2020-07-23 16:23:10 -0700

    add show controller method, for getting pedal by id

--- app/controllers/pedals_controller.rb
+++ app/controllers/pedals_controller.rb
@@ -4,4 +4,15 @@ class PedalsController < ApplicationController
     json_response(@pedals)
   end
 
+  def show
+    @pedal = Pedal.find(params[:id])
+    json_response(@pedal)
+  end
+
+
+  private
+  def pedal_params
+    params.permit(:name, :price, :brand, :description, :quantity, :featured, :image_url, :kind, :category, :country_origin)
+  end
+
 end
